25bce506d79c7984ad4f3e87f8428156c8ba7238
virattt <virat.dot@gmail.com>
2025-10-16 17:10:31 -0700

    Create filings tools

---
+++ src/dexter/tools/__init__.py
@@ -0,0 +1,18 @@
+from typing_extensions import Callable
+from dexter.tools.financials import get_income_statements
+from dexter.tools.financials import get_balance_sheets
+from dexter.tools.financials import get_cash_flow_statements
+from dexter.tools.filings import get_filings
+from dexter.tools.filings import get_10K_filing_items
+from dexter.tools.filings import get_10Q_filing_items
+from dexter.tools.filings import get_8K_filing_items
+
+TOOLS: list[Callable[..., any]] = [
+    get_income_statements,
+    get_balance_sheets,
+    get_cash_flow_statements,
+    get_10K_filing_items,
+    get_10Q_filing_items,
+    get_8K_filing_items,
+    get_filings,
+]
