9873680b6fd487057e37c6dc488904c185d5bd26
virattt <virat.dot@gmail.com>
2025-10-21 19:47:30 -0400

    Add stock prices

--- src/dexter/tools/__init__.py
+++ src/dexter/tools/__init__.py
@@ -6,6 +6,8 @@ from dexter.tools.filings import get_filings
 from dexter.tools.filings import get_10K_filing_items
 from dexter.tools.filings import get_10Q_filing_items
 from dexter.tools.filings import get_8K_filing_items
+from dexter.tools.prices import get_price_snapshot
+from dexter.tools.prices import get_prices
 
 TOOLS: list[Callable[..., any]] = [
     get_income_statements,
@@ -15,4 +17,6 @@ TOOLS: list[Callable[..., any]] = [
     get_10Q_filing_items,
     get_8K_filing_items,
     get_filings,
+    get_price_snapshot,
+    get_prices,
 ]
